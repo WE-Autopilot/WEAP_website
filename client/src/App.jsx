6f077bc3776924fbaa00e3f0c6da729245fd7bdb
nshang3 <nshang3@uwo.ca>
2024-12-27 22:46:26 -0500

    WIP Headers component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./components/Home"
 import './App.css'
 
 function App() {
@@ -8,9 +9,13 @@ function App() {
 
   return (
     <Router>
-
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about"/>
+        <Route path="/sponsors" />
+        <Route path="/join" />
+        <Route path="/competition" />
 
       </Routes>
     </Router>
