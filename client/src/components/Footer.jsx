23f159e97ee07daf7b1b0714d412724735d5194e
Malcolm Fonseca <113556151+MalcolmFonseca@users.noreply.github.com>
2025-01-09 20:51:17 -0500

    formatted footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,13 +5,17 @@ function Footer() {
     <footer className="Footer">
       <div id="FooterLeft">
         <img src="/socials/footerlogo.png" id="FooterLogo" width="215px"></img>
-        <p id="Address">1151 Richmond St, London, ON N6A 3K7</p>
+        <p id="Address">
+          1151 Richmond St,
+          <br />
+          London, ON N6A 3K7
+        </p>
       </div>
       <div id="Socials">
         <p id="SocialTitle">Socials</p>
-        <div id="SocialImgs">
+        <div id="SocialImages">
           <img src="/socials/linktree.png" width="52px"></img>
-          <img src="/socials/instagram.png" width="85px"></img>
+          <img src="/socials/instagram.png" width="40px"></img>
           <img src="/socials/linkedin.png" width="46px"></img>
           <img src="/socials/discord.png" width="53px"></img>
         </div>
