eff473b1b5968a035f8523df00adb9efa3172b30
NoorSahota <harnoor_sahota@hotmail.com>
2025-01-11 17:41:45 -0500

    added the html and svg logos

--- client/src/components/Sponsors.jsx
+++ client/src/components/Sponsors.jsx
@@ -1,2 +1,18 @@
-// This is the sponsors.jsx file
-// my name is noor
+function Sponsors() {
+
+    return (
+        <div className = "Sponsors">
+            <h1 className = "heading">Our Sponsors</h1>
+            <div className = "logos">
+                <div className = "sponsor">
+                    <img src = "client\public\sponsors.svg\ROBOSHOP_LOGO 1.svg" alt = "RobotShop Logo" className = "logo"/>
+                </div>
+                <div className = "sponsor">
+                    <img src = "client\public\sponsors.svg\digikey 1.svg" alt = "Digikey" className = "logo"/>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
