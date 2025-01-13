e1d8dd56f3d96a4da7dd9cf0b23f28700212ca8c
NoorSahota <harnoor_sahota@hotmail.com>
2025-01-13 17:45:14 -0500

    made some changes to html

--- client/src/components/Sponsors.jsx
+++ client/src/components/Sponsors.jsx
@@ -5,10 +5,10 @@ function Sponsors() {
             <h1 className = "heading">Our Sponsors</h1>
             <div className = "logos">
                 <div className = "sponsor">
-                    <img src = "client\public\sponsors.svg\ROBOSHOP_LOGO 1.svg" alt = "RobotShop Logo" className = "logo"/>
+                    <img src = "client\public\sponsors.svg\ROBOSHOP_LOGO 1.svg" alt = "RobotShop Logo" className = "sponsor-logo"/>
                 </div>
                 <div className = "sponsor">
-                    <img src = "client\public\sponsors.svg\digikey 1.svg" alt = "Digikey" className = "logo"/>
+                    <img src = "client\public\sponsors.svg\digikey 1.svg" alt = "Digikey" className = "sponsor-logo"/>
                 </div>
             </div>
         </div>
