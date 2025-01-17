c8197cd3960c6e2c4f3db53debf820d1d7fdf8e3
NoorSahota <harnoor_sahota@hotmail.com>
2025-01-16 19:46:20 -0500

    added css styling

--- client/src/components/Sponsors.jsx
+++ client/src/components/Sponsors.jsx
@@ -1,3 +1,5 @@
+import '../stylesheets/Header.css'
+
 function Sponsors() {
 
     return (
@@ -5,10 +7,10 @@ function Sponsors() {
             <h1 className = "heading">Our Sponsors</h1>
             <div className = "logos">
                 <div className = "sponsor">
-                    <img src = "client\public\sponsors.svg\ROBOSHOP_LOGO 1.svg" alt = "RobotShop Logo" className = "sponsor-logo"/>
+                    <img src = "client\public\sponsors.svg\ROBOSHOP_LOGO 1.svg" alt = "RobotShop Logo" className = "roboshop-logo"/>
                 </div>
                 <div className = "sponsor">
-                    <img src = "client\public\sponsors.svg\digikey 1.svg" alt = "Digikey" className = "sponsor-logo"/>
+                    <img src = "client\public\sponsors.svg\digikey 1.svg" alt = "Digikey" className = "digikey-logo"/>
                 </div>
             </div>
         </div>
@@ -16,3 +18,4 @@ function Sponsors() {
 
 }
 
+export default Sponsors;
