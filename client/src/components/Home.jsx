6c4d8066c28f48e9d6028288ad8108113dfeab46
Aaronsiby15 <aaronsiby05@gmail.com>
2025-01-22 13:14:45 -0500

    HomeSection

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,29 +3,17 @@ import "../stylesheets/Home.css";
 function Home() {
   return (
     <div className="home-container">
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-      <h1>WEAP</h1>
-
+    <section id="about-we-autopilot" className="about-container">
+  <div className="content">
+    <h2>What is WE AutoPilot?</h2>
+    <p>
+    Welcome to the official Western Engineering AutoPilot Website! We are a group of driven engineers and developers, looking to change the world with our Autonomy Software, one road at a time. Feel free to explore everything, from our current endeavours, to future aspirations.
+    </p>
+  </div>
+</section>
     </div>
   );
 }
 
 export default Home;
+
